0740cbde3010deb565373130eb7ee4c7c9a9beed
Samuel <samueljoseantunez@gmail.com>
2026-04-08 20:04:56 -0400

    env

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -1,9 +1,9 @@
 import 'dotenv/config'
 
-import { get } from 'env-var'
+import env from 'env-var'
 
 export const envs = {
-    PORT: get('PORT').required().asPortNumber(),
-    PUBLIC_PATH: get('PUBLIC_PATH').default('public').asString(),
-    POSTGRES_URL: get('POSTGRES_URL').required().asString(),
+    PORT: env.get('PORT').required().asPortNumber(),
+    PUBLIC_PATH: env.get('PUBLIC_PATH').default('public').asString(),
+    POSTGRES_URL: env.get('POSTGRES_URL').required().asString(),
 }
